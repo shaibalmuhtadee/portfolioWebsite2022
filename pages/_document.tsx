1b5a84bcba0d9981f9f59afa858c9ac6f47e2f03
Shaibal Muhtadee <shaibalmuhtadee@gmail.com>
2022-06-07 02:13:04 -0400

    added favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,8 @@ export default function Document() {
       <Head>
         <link rel="preconnect" href="https://fonts.gstatic.com"/>
         <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet"/>
+        <link rel="icon" type="image/svg+xml" href="/favicon/favicon.svg"/>
+        <link rel="icon" type="image/png" href="/favicon/favicon.png"/>
       </Head>
       <body className='bg-light_primary dark:bg-dark_primary'>
         <Main />
