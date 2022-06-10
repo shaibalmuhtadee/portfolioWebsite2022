d0e04cd6d0c77207b05d61f41522b6741b367cc4
Shaibal Muhtadee <shaibalmuhtadee@gmail.com>
2022-06-10 18:57:51 -0400

    added animations to all sections

--- components/intro/intro.tsx
+++ components/intro/intro.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Emoji from './emoji'
 import Link from 'next/link'
+import { motion } from "framer-motion"
 import { FaGithub, FaLinkedin } from "react-icons/fa";
 import { GrMail } from "react-icons/gr";
 
@@ -8,15 +9,15 @@ const intro = () => {
    return (
       <div className='px-6 pt-4 text-black cursor-default md:pt-24 dark:text-light_primary font-Rubik' id="intro">
          
-         <div className='rounded-full w-44 h-44 md:w-52 md:h-52 ring ring-black dark:ring-light_primary'>
+         <motion.div initial={{y: -30, opacity: 0}} whileInView={{y: 0, opacity: 1, transition: {duration: 0.75}}} className='rounded-full w-44 h-44 md:w-52 md:h-52 ring ring-black dark:ring-light_primary'>
             <img className='rounded-full w-44 h-44 md:w-52 md:h-52' src="/picture.gif" alt="author"/>
-         </div>
+         </motion.div>
 
-         <h1 className='flex-wrap px-2 pt-8 pb-2 text-5xl font-bold md:pt-12 md:flex'>Hi, I&apos;m Shai!&nbsp;<span className='inline-block'><Emoji symbol='👋' label='wave' /></span></h1>
+         <motion.h1 initial={{y: -30, opacity: 0}} whileInView={{y: 0, opacity: 1, transition: {duration: 0.75}}} className='flex-wrap px-2 pt-8 pb-2 text-5xl font-bold md:pt-12 md:flex'>Hi, I&apos;m Shai!&nbsp;<span className='inline-block'><Emoji symbol='👋' label='wave' /></span></motion.h1>
       
-         <h2 className='px-2 py-5 text-3xl font-bold md:text-4xl text-light_gray_custom dark:text-dark_gray_custom'>I am an aspiring web developer and computer engineering student at the University of Toronto seeking an internship this summer, check out my <span className='inline-block cursor-pointer dark:text-dark_highlight text-light_highlight link-underline link-underline-highlight'><a href=''>resume</a></span></h2>
+         <motion.h2 initial={{y: -30, opacity: 0}} whileInView={{y: 0, opacity: 1, transition: {duration: 0.75}}} className='px-2 py-5 text-3xl font-bold md:text-4xl text-light_gray_custom dark:text-dark_gray_custom'>I am an aspiring web developer and computer engineering student at the <span className='inline-block cursor-pointer bg-light_highlight/40 dark:bg-dark_highlight/30 link-underline link-underline-highlight'><a href='https://www.utoronto.ca/'>University of Toronto</a></span> seeking an internship this summer, check out my <span className='inline-block cursor-pointer dark:text-dark_highlight text-light_highlight link-underline link-underline-highlight'><a href=''>resume</a></span></motion.h2>
          
-         <div className='flex px-2 space-x-5 text-4xl cursor-pointer'>
+         <motion.div initial={{y: -30, opacity: 0}} whileInView={{y: 0, opacity: 1, transition: {duration: 0.75}}} className='flex px-2 space-x-5 text-4xl cursor-pointer'>
             <div className='transition-transform duration-150 ease-in-out hover:scale-110 hover:rotate-12'>
                <Link href="https://github.com/shaibalmuhtadee"><a><FaGithub /></a></Link>
             </div>
@@ -28,11 +29,13 @@ const intro = () => {
             <div className='transition-transform duration-150 ease-in-out text-3-5xl hover:scale-110 hover:rotate-12'>
                <Link href="mailto: shaibalmuhtadee@gmail.com"><a><GrMail /></a></Link>
             </div>
-         </div>
+         </motion.div>
          
-         <h3 className='px-2 pt-5 text-2xl font-semibold text-light_gray_custom dark:text-dark_gray_custom'>I love learning the logic behind complex software programs and developing powerful interactive apps.</h3>
-         <h3 className='px-2 pt-5 text-2xl font-semibold text-light_gray_custom dark:text-dark_gray_custom'>My technology stack includes:&nbsp;<span className='inline-block transition-colors duration-200 ease-in hover:text-react_color'>React</span>,&nbsp;<span className='inline-block transition-colors duration-200 ease-in hover:text-typescript_color'>TypeScript</span>,&nbsp;<span className='inline-block transition-colors duration-200 ease-in hover:text-graphql_color'>GraphQL</span>,&nbsp;<span className='inline-block transition-colors duration-200 ease-in hover:text-python_color'>Python</span>,&nbsp;<span className='inline-block transition-colors duration-200 ease-in hover:text-cplusplus_color'>C++</span></h3>
-      
+         <motion.div initial={{y: -30, opacity: 0}} whileInView={{y: 0, opacity: 1, transition: {duration: 0.75}}}>
+            <h3 className='px-2 pt-5 text-2xl font-semibold text-light_gray_custom dark:text-dark_gray_custom'>I love learning the logic behind complex software programs and developing powerful interactive apps.</h3>
+            <h3 className='px-2 pt-5 text-2xl font-semibold text-light_gray_custom dark:text-dark_gray_custom'>My technology stack includes:&nbsp;<span className='inline-block transition-colors duration-200 ease-in hover:text-react_color'>React</span>,&nbsp;<span className='inline-block transition-colors duration-200 ease-in hover:text-typescript_color'>TypeScript</span>,&nbsp;<span className='inline-block transition-colors duration-200 ease-in hover:text-graphql_color'>GraphQL</span>,&nbsp;<span className='inline-block transition-colors duration-200 ease-in hover:text-python_color'>Python</span>,&nbsp;<span className='inline-block transition-colors duration-200 ease-in hover:text-cplusplus_color'>C++</span></h3>
+         </motion.div>
+
       </div>
    )
 }
--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -4,17 +4,19 @@ import { motion } from "framer-motion"
 const Projects = () => {
    return (
       <div className='px-6 pt-4 text-black cursor-default md:pt-12 dark:text-light_primary font-Rubik' id="projects">
-         <h2 className='px-2 py-5 text-4xl font-bold'>Projects</h2>
-         <h3 className='px-2 pt-5 text-2xl font-semibold text-light_gray_custom dark:text-dark_gray_custom'>Here are my most recent projects that I enjoyed making and designing from scratch, both as a team and alone.</h3>
+         <motion.div initial={{y: -30, opacity: 0}} whileInView={{y: 0, opacity: 1, transition: {duration: 0.75}}}>
+            <h2 className='px-2 py-5 text-4xl font-bold'>Projects</h2>
+            <h3 className='px-2 pt-5 text-2xl font-semibold text-light_gray_custom dark:text-dark_gray_custom'>Here are my most recent projects that I enjoyed making and designing from scratch, both as a team and alone.</h3>
+         </motion.div>
          <div className='pt-12 mx-2 space-y-14'>
             
-            <motion.div initial={{x: -100, opacity: 0}} whileInView={{x: 0, opacity: 1}} transition={{ease: "easeOut", duration: 1}} className='w-full rounded-md -z-10 h-[22rem] bg-slate-400 drop-shadow-lg'></motion.div>
+            <motion.div initial={{x: -50, opacity: 0}} whileInView={{x: 0, opacity: 1}} transition={{ease: "easeOut", duration: 1}} className='w-full rounded-md -z-10 h-[22rem] bg-slate-400 drop-shadow-lg'></motion.div>
 
-            <div className='w-full rounded-md -z-10 h-[22rem] bg-cyan-400 drop-shadow-lg'></div>
+            <motion.div initial={{x: 50, opacity: 0}} whileInView={{x: 0, opacity: 1}} transition={{ease: "easeOut", duration: 1}} className='w-full rounded-md -z-10 h-[22rem] bg-cyan-400 drop-shadow-lg'></motion.div>
 
-            <div className='w-full bg-orange-400 rounded-md -z-10 h-[22rem] drop-shadow-lg'></div>
+            <motion.div initial={{x: -50, opacity: 0}} whileInView={{x: 0, opacity: 1}} transition={{ease: "easeOut", duration: 1}} className='w-full bg-orange-400 rounded-md -z-10 h-[22rem] drop-shadow-lg'></motion.div>
 
-            <div className='w-full bg-green-400 rounded-md -z-10 h-[22rem] drop-shadow-lg'></div>
+            <motion.div initial={{x: 50, opacity: 0}} whileInView={{x: 0, opacity: 1}} transition={{ease: "easeOut", duration: 1}} className='w-full bg-green-400 rounded-md -z-10 h-[22rem] drop-shadow-lg'></motion.div>
          
          </div>
       </div>
