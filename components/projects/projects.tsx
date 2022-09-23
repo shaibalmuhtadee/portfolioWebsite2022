01669d5b35adccc9af0cbb6feed1473052f1efa4
Shaibal Muhtadee <shaibal.muhtadee@mail.utoronto.ca>
2022-09-23 12:57:54 -0400

    added new project

--- components/projects/projects.tsx
+++ components/projects/projects.tsx
@@ -9,7 +9,30 @@ const Projects = () => {
             <h3 className='px-2 pt-5 text-2xl font-semibold text-light_gray_custom dark:text-dark_gray_custom'>Here are my most recent projects that I enjoyed making and designing from scratch, both as a team and alone.</h3>
          </motion.div>
          <div className='pt-12 mx-2 space-y-14'>
- 
+
+            <motion.div 
+               initial={{x: -50, opacity: 0}} 
+               whileInView={{x: 0, opacity: 1}} 
+               transition={{ease: "easeInOut", duration: 0.5}} 
+               className='w-full rounded-md -z-10 h-[22rem] drop-shadow-lg grid grid-cols-2'
+            >
+               <div className='flex flex-col items-center justify-center w-full h-full col-span-1 text-left'>
+                  <h1 className='pt-6 pb-2 pl-10 mr-auto text-4xl font-bold uppercase'>Inventory Dashboard</h1>
+                  <h2 className='pl-10 text-2xl font-extrabold text-light_gray_custom dark:text-dark_gray_custom'>Full Stack App for managing inventory data</h2>
+                  <div className='z-50 text-[16px] flex items-center w-full p-4 mt-8 ml-20 text-left rounded-md shadow-xl text-light_gray_custom dark:text-dark_gray_custom h-26 bg-light_primary dark:bg-dark_primary'>
+                     <h1>Currently designing and building a functional dashboard application to keep track of the inventory status of a restaurant</h1>
+                  </div>
+                  <div className='flex pb-4 mt-auto mr-auto space-x-2 text-xl text-light_gray_custom dark:text-dark_gray_custom pl-11'>
+                     <h1>React</h1>
+                     <h1>MongoDB</h1>
+                     <h1>GraphQL</h1>
+                  </div>
+               </div>
+               <motion.div whileHover={{y:-7, z:-1, transition: {duration: 0.2, ease: "easeInOut" }}} className='flex items-center justify-center w-full h-full col-span-1 text-center rounded-md cursor-pointer dark:bg-light_primary bg-dark_primary'>
+                  <img className='w-auto h-72' src="/projectIcons/dashboard.png" alt="icon" />
+               </motion.div>
+            </motion.div>
+
             <motion.div 
                initial={{x: -50, opacity: 0}} 
                whileInView={{x: 0, opacity: 1}} 
@@ -17,7 +40,7 @@ const Projects = () => {
                className='w-full rounded-md -z-10 h-[22rem] drop-shadow-lg grid grid-cols-2'
             >
                <motion.div whileHover={{y:-7, z:-1, transition: {duration: 0.2, ease: "easeInOut" }}} className='flex items-center justify-center w-full h-full col-span-1 text-center rounded-md cursor-pointer dark:bg-light_primary bg-dark_primary'>
-                  <img className='w-72 h-72' src="/music.png" alt="icon" />
+                  <img className='w-72 h-72' src="/projectIcons/music.png" alt="icon" />
                </motion.div>
                <div className='flex flex-col items-center justify-center w-full h-full col-span-1 text-right'>
                   <h1 className='pt-6 pb-2 pr-10 ml-auto text-4xl font-bold uppercase'>Jukebox</h1>
@@ -52,7 +75,7 @@ const Projects = () => {
                   </div>
                </div>
                <motion.div whileHover={{y:-7, z:-1, transition: {duration: 0.2, ease: "easeInOut" }}} className='flex items-center justify-center w-full h-full col-span-1 text-center rounded-md cursor-pointer dark:bg-light_primary bg-dark_primary'>
-                  <img className='w-auto h-72' src="/todolist.png" alt="icon" />
+                  <img className='w-auto h-72' src="/projectIcons/todolist.png" alt="icon" />
                </motion.div>
             </motion.div>
 
@@ -63,7 +86,7 @@ const Projects = () => {
                className='w-full rounded-md -z-10 h-[22rem] drop-shadow-lg grid grid-cols-2'
             >
                <motion.div whileHover={{y:-7, z:-1, transition: {duration: 0.2, ease: "easeInOut" }}} className='flex items-center justify-center w-full h-full col-span-1 text-center rounded-md cursor-pointer dark:bg-light_primary bg-dark_primary'>
-                  <img className='w-auto h-max-72' src="/chromamap.png" alt="icon" />
+                  <img className='w-auto h-max-72' src="/projectIcons/chromamap.png" alt="icon" />
                </motion.div>
                <div className='flex flex-col items-center justify-center w-full h-full col-span-1 text-right'>
                   <h1 className='pt-6 pb-2 pr-10 ml-auto text-4xl font-bold'>ChromaMap</h1>
@@ -98,7 +121,7 @@ const Projects = () => {
                   </div>
                </div>
                <motion.div whileHover={{y:-7, z:-1, transition: {duration: 0.2, ease: "easeInOut" }}} className='flex items-center justify-center w-full h-full col-span-1 text-center rounded-md cursor-pointer dark:bg-light_primary bg-dark_primary'>
-                  <img className='h-72' src="/arbis.png" alt="icon" />
+                  <img className='h-72' src="/projectIcons/arbis.png" alt="icon" />
                </motion.div>
             </motion.div>
 
